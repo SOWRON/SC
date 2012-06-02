8e50ed8774828d35fd772b7b64ffaee0b38815fc
Wazy <biobud74@yahoo.com>
2012-06-02 09:49:04 +0200

    I have no comment for this.. reverting "Fixed a major bug."
    This reverts commit 858ad927504806307effc9b898e152699238605b.

---
+++ src/server/scripts/EasternKingdoms/ElwynnForest/elwynnforest.cpp
@@ -0,0 +1,276 @@
+/*
+ * Copyright (C) 2011-2012 Project SkyFire <http://www.projectskyfire.org/>
+ * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2006-2012 ScriptDev2 <https://scriptdev2.svn.sourceforge.net/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* ScriptData
+SDName: Elwynn_Forest
+SD%Complete: 50
+SDComment: Quest support: 1786
+SDCategory: Elwynn Forest
+EndScriptData */
+
+/* ContentData
+npc_henze_faulk
+EndContentData */
+
+#include "ScriptPCH.h"
+
+/*######
+## npc_henze_faulk
+######*/
+enum eHenzeFaulkData
+{
+    SAY_HEAL = -1000187,
+};
+
+class npc_henze_faulk : public CreatureScript
+{
+public:
+    npc_henze_faulk() : CreatureScript("npc_henze_faulk") { }
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_henze_faulkAI (creature);
+    }
+
+    struct npc_henze_faulkAI : public ScriptedAI
+    {
+        uint32 lifeTimer;
+        bool spellHit;
+
+        npc_henze_faulkAI(Creature* creature) : ScriptedAI(creature) {}
+
+        void Reset()
+        {
+            lifeTimer = 120000;
+            me->SetUInt32Value(UNIT_DYNAMIC_FLAGS, UNIT_DYNFLAG_DEAD);
+            me->SetStandState(UNIT_STAND_STATE_DEAD);   // lay down
+            spellHit = false;
+        }
+
+        void EnterCombat(Unit * /*who*/)
+        {
+        }
+
+        void MoveInLineOfSight(Unit * /*who*/)
+        {
+        }
+
+        void UpdateAI(const uint32 diff)
+        {
+            if (me->IsStandState())
+            {
+                if (lifeTimer <= diff)
+                {
+                    EnterEvadeMode();
+                    return;
+                }
+                else
+                    lifeTimer -= diff;
+            }
+        }
+
+        void SpellHit(Unit * /*Hitter*/, const SpellEntry *Spellkind)
+        {
+            if (Spellkind->Id == 8593 && !spellHit)
+            {
+                DoCast(me, 32343);
+                me->SetStandState(UNIT_STAND_STATE_STAND);
+                me->SetUInt32Value(UNIT_DYNAMIC_FLAGS, 0);
+                //me->RemoveAllAuras();
+                DoScriptText(SAY_HEAL, me);
+                spellHit = true;
+            }
+        }
+    };
+};
+
+enum Northshire
+{
+    SAY_BLACKROCK_COMBAT_1    = -1000015,
+    SAY_BLACKROCK_COMBAT_2    = -1000016,
+    SAY_BLACKROCK_COMBAT_3    = -1000017,
+    SAY_BLACKROCK_COMBAT_4    = -1000018,
+    SAY_BLACKROCK_COMBAT_5    = -1000019,
+    SAY_ASSASSIN_COMBAT_1     = -1000020,
+    SAY_ASSASSIN_COMBAT_2     = -1000021,
+    SPELL_SPYING              = 92857,
+    SPELL_SNEAKING            = 93046,
+    SPELL_SPYGLASS            = 80676,
+};
+
+/*######
+## npc_blackrock_spy
+######*/
+
+class npc_blackrock_spy : public CreatureScript
+{
+public:
+    npc_blackrock_spy() : CreatureScript("npc_blackrock_spy") { }
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_blackrock_spyAI (creature);
+    }
+
+    struct npc_blackrock_spyAI : public ScriptedAI
+    {
+        npc_blackrock_spyAI(Creature* creature) : ScriptedAI(creature)
+        {
+            CastSpying();
+        }
+
+        void CastSpying()
+        {
+            GetCreature(-8868.88f, -99.1016f);
+            GetCreature(-8936.5f, -246.743f);
+            GetCreature(-8922.44f, -73.9883f);
+            GetCreature(-8909.68f, -40.0247f);
+            GetCreature(-8834.85f, -119.701f);
+            GetCreature(-9022.08f, -163.965f);
+            GetCreature(-8776.55f, -79.158f);
+            GetCreature(-8960.08f, -63.767f);
+            GetCreature(-8983.12f, -202.827f);
+        }
+
+        void GetCreature(float X, float Y)
+        {
+            if (me->GetHomePosition().GetPositionX() == X, me->GetHomePosition().GetPositionY() == Y)
+                if (!me->isInCombat() && !me->HasAura(SPELL_SPYING))
+                    DoCast(me, SPELL_SPYING);
+
+            CastSpyglass();
+        }
+
+        void CastSpyglass()
+        {
+            Spyglass(-8868.88f, -99.1016f, -8936.5f, -246.743f, -8922.44f, -73.9883f, -8909.68f, -40.0247f, -8834.85f,
+                -119.701f, -9022.08f, -163.965f, -8776.55f, -79.158f, -8960.08f, -63.767f, -8983.12f, -202.827f);
+        }
+
+        void Spyglass(float X1, float Y1, float X2, float Y2, float X3, float Y3, float X4, float Y4, float X5, float Y5,
+            float X6, float Y6, float X7, float Y7, float X8, float Y8, float X9, float Y9)
+        {
+            if (me->GetHomePosition().GetPositionX() != X1, me->GetHomePosition().GetPositionY() != Y1)
+            if (me->GetHomePosition().GetPositionX() != X2, me->GetHomePosition().GetPositionY() != Y2)
+            if (me->GetHomePosition().GetPositionX() != X3, me->GetHomePosition().GetPositionY() != Y3)
+            if (me->GetHomePosition().GetPositionX() != X4, me->GetHomePosition().GetPositionY() != Y4)
+            if (me->GetHomePosition().GetPositionX() != X5, me->GetHomePosition().GetPositionY() != Y5)
+            if (me->GetHomePosition().GetPositionX() != X6, me->GetHomePosition().GetPositionY() != Y6)
+            if (me->GetHomePosition().GetPositionX() != X7, me->GetHomePosition().GetPositionY() != Y7)
+            if (me->GetHomePosition().GetPositionX() != X8, me->GetHomePosition().GetPositionY() != Y8)
+            if (me->GetHomePosition().GetPositionX() != X9, me->GetHomePosition().GetPositionY() != Y9)
+                if (me->GetHomePosition().GetPositionX() == me->GetPositionX(), me->GetHomePosition().GetPositionY() == me->GetPositionY())
+                    if (!me->isInCombat() && !me->HasAura(SPELL_SPYGLASS))
+                        DoCast(me, SPELL_SPYGLASS);
+        }
+
+        void EnterCombat(Unit * who)
+        {
+            DoScriptText(RAND(SAY_BLACKROCK_COMBAT_1, SAY_BLACKROCK_COMBAT_2, SAY_BLACKROCK_COMBAT_3, SAY_BLACKROCK_COMBAT_4, SAY_BLACKROCK_COMBAT_5), me);
+        }
+
+        void UpdateAI(const uint32 diff)
+        {
+            CastSpyglass();
+
+            if (!UpdateVictim())
+                return;
+
+            DoMeleeAttackIfReady();
+        }
+    };
+};
+
+/*######
+## npc_blackrock_invader
+######*/
+
+class npc_blackrock_invader : public CreatureScript
+{
+public:
+    npc_blackrock_invader() : CreatureScript("npc_blackrock_invader") { }
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_blackrock_invaderAI (creature);
+    }
+
+    struct npc_blackrock_invaderAI : public ScriptedAI
+    {
+        npc_blackrock_invaderAI(Creature* creature) : ScriptedAI(creature) {}
+
+        void EnterCombat(Unit * who)
+        {
+            DoScriptText(RAND(SAY_BLACKROCK_COMBAT_1, SAY_BLACKROCK_COMBAT_2, SAY_BLACKROCK_COMBAT_3, SAY_BLACKROCK_COMBAT_4, SAY_BLACKROCK_COMBAT_5), me);
+        }
+
+        void UpdateAI(const uint32 diff)
+        {
+            if (!UpdateVictim())
+                return;
+
+            DoMeleeAttackIfReady();
+        }
+    };
+};
+
+/*######
+## npc_goblin_assassin
+######*/
+
+class npc_goblin_assassin : public CreatureScript
+{
+public:
+    npc_goblin_assassin() : CreatureScript("npc_goblin_assassin") { }
+
+    CreatureAI* GetAI(Creature* creature) const
+    {
+        return new npc_goblin_assassinAI (creature);
+    }
+
+    struct npc_goblin_assassinAI : public ScriptedAI
+    {
+        npc_goblin_assassinAI(Creature* creature) : ScriptedAI(creature)
+        {
+            if (!me->isInCombat() && !me->HasAura(SPELL_SPYING))
+                DoCast(SPELL_SNEAKING);
+        }
+
+        void EnterCombat(Unit * who)
+        {
+            DoScriptText(RAND(SAY_ASSASSIN_COMBAT_1, SAY_ASSASSIN_COMBAT_2), me);
+        }
+
+        void UpdateAI(const uint32 diff)
+        {
+            if (!UpdateVictim())
+                return;
+
+            DoMeleeAttackIfReady();
+        }
+    };
+};
+
+void AddSC_elwynn_forest()
+{
+    new npc_henze_faulk();
+    new npc_blackrock_spy();
+    new npc_goblin_assassin();
+    new npc_blackrock_invader();
+}
